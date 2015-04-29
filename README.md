b31eb3525f38a0a9916fa740c11cc6a5475900c4
ambrozkovapetra <ambrozkovapetra@gmail.com>
2015-04-29 21:56:10 +0200

    Update

--- README.md
+++
@@ -1,5 +0,0 @@
-Esper
-
-- program vypisuje náhodně znak a, b nebo c, aktuální datum a čas
-- problém v EPL statementu - jakou metodu použít pro výběr např. pouze znaku a? 
-  (EPL statement v kódu zachytí jen dvě první dvě události..)
--- src/main/java/com/esper/app/exampleMain.java
+++ src/main/java/com/esper/app/exampleMain.java
@@ -10,31 +10,26 @@ import org.apache.log4j.Logger;
  */
 import com.espertech.esper.client.*;
 import java.util.Random;
-import java.util.Date;
 
 public class exampleMain {
  
     public static class Tick {
         Random r = new Random();
         String symbol;
-        //Double price;
-        Date timeStamp;
         Character alph;
         
  
-        public Tick(String s, char a, long t) {
+        public Tick(String s, char a) {
             symbol = s;
             alph = a;
-            timeStamp = new Date(t);
-            
         }
+        
         public double getAplh() {return alph;}
         public String getSymbol() {return symbol;}
-        public Date getTimeStamp() {return timeStamp;}
  
         @Override
         public String toString() {
-            return "Character: " + alph.toString() + " time: " + timeStamp.toString();
+            return "Character " + alph.toString();
         }
     }
  
@@ -42,30 +37,22 @@ public class exampleMain {
  
     public static void GenerateRandomTick(EPRuntime cepRT) {
         String alphabet = "abc";
-        //double price = (double) generator.nextInt(10);
         char alph = (char) alphabet.charAt(generator.nextInt(alphabet.length()));
-        long timeStamp = System.currentTimeMillis();
         String symbol = "AAPL";
-        Tick tick = new Tick(symbol, alph, timeStamp);
-        System.out.println("Sending tick:" + tick);
+        Tick tick = new Tick(symbol, alph);
         cepRT.sendEvent(tick);
- 
     }
  
     public static class CEPListener implements UpdateListener {
  
         public void update(EventBean[] newData, EventBean[] oldData) {
-            System.out.println("Event received: " + newData[0].getUnderlying());
+            System.out.println("Event: " + newData[0].getUnderlying());
         }
     }
  
     public static void main(String[] args) {
- 
-        SimpleLayout layout = new SimpleLayout();
-        ConsoleAppender appender = new ConsoleAppender(new SimpleLayout());
-        Logger.getRootLogger().addAppender(appender);
-        Logger.getRootLogger().setLevel((Level) Level.WARN);
-//The Configuration is meant only as an initialization-time object.
+           
+        //The Configuration is meant only as an initialization-time object.
         Configuration cepConfig = new Configuration();
         cepConfig.addEventType("StockTick", Tick.class.getName());
         EPServiceProvider cep = EPServiceProviderManager.getProvider("myCEPEngine", cepConfig);
@@ -73,13 +60,12 @@ public class exampleMain {
  
         EPAdministrator cepAdm = cep.getEPAdministrator();
         EPStatement cepStatement = cepAdm.createEPL("select * from " +
-                "StockTick(symbol='AAPL').win:firstlength(2)");
+                "StockTick");
                 
- 
         cepStatement.addListener(new CEPListener());
  
        // We generate a few ticks...
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 10; i++) {
             GenerateRandomTick(cepRT);
         }
     }
