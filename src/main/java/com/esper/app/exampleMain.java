12e57cf054e7f4924213e7e98f0e3acb18a79f0e
ambrozkovapetra <ambrozkovapetra@gmail.com>
2015-05-05 13:02:04 +0200

    Update exampleMain.java

--- src/main/java/com/esper/app/exampleMain.java
+++ src/main/java/com/esper/app/exampleMain.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
 
-public class exampleMain {
+public class ExampleMain {
  
     public static class Tick {
         Random r = new Random();
